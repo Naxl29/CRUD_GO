1cfd457468e7a31cad23470ac84b99c15b5036f1
DESKTOP-2Q8H8VU\User <deysonurrego159@gmail.com>
2025-10-17 20:40:04 -0500

    solucion a error indices nulos

--- models/Persona.go
+++ models/Persona.go
@@ -9,7 +9,7 @@ type Persona struct {
 	PrimerApellido  string `gorm:"type:varchar(50);not null" json:"primer_apellido"`
 	SegundoApellido string `gorm:"type:varchar(50)" json:"segundo_apellido"`
 	Documento       string `gorm:"type:varchar(20);unique;not null" json:"documento"`
-	Correo          string `gorm:"type:varchar(100);unique" json:"correo"`
-	Direccion       string `gorm:"type:varchar(200)" json:"direccion"`
-	Telefono        string `gorm:"type:varchar(15)" json:"telefono"`
+	Correo          string `gorm:"type:varchar(100);unique;not null" json:"correo"`
+	Direccion       string `gorm:"type:varchar(200);not null" json:"direccion"`
+	Telefono        string `gorm:"type:varchar(15);not null" json:"telefono"`
 }
